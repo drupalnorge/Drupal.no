c23f27a97d38750d673eeaf98f5fdbb9f3eb6518
Frank Gjertsen <frank@rabbeldesign.no>
2012-04-16 11:55:04 +0200

    Position footer at bottom, close gh-12

--- sites/all/themes/dno/templates/page.tpl.php
+++ sites/all/themes/dno/templates/page.tpl.php
@@ -69,88 +69,88 @@
  */
 ?>
 <?php if ($logo || $site_name || $site_slogan || $page['header']): ?>
-
-  <?php if ($page['header_top']): ?>
-      <div id="header_top">
-        <div class="wrapper clearfix">
-          <?php print render($page['header_top']); ?>
+  <div class="page-wrapper">
+    <?php if ($page['header_top']): ?>
+        <div id="header_top">
+          <div class="wrapper clearfix">
+            <?php print render($page['header_top']); ?>
+          </div>
         </div>
-      </div>
-  <?php endif; ?>
+    <?php endif; ?>
 
-  <header id="header" role="banner">
-    <div class="wrapper"><div class="inner">
-      <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-        </a>
+    <header id="header" role="banner">
+      <div class="wrapper"><div class="inner">
+        <?php if ($logo): ?>
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+          </a>
+        <?php endif; ?>
+
+        <?php if ($site_name): ?>
+            <h1 id="site-name">
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+            </h1>
+        <?php endif; ?>
+
+        <?php if ($site_slogan): ?>
+          <div id="site-slogan"><?php print $site_slogan; ?></div>
+        <?php endif; ?>
+
+        <?php print render($page['header']); ?>
+      </header><!-- /#header -->
+    <?php endif; ?>
+
+    <div id="page">
+      <?php if ($page['featured']): ?>
+        <div id="featured">
+          <?php print render($page['featured']); ?>
+        </div><!-- /#featured -->
       <?php endif; ?>
 
-      <?php if ($site_name): ?>
-          <h1 id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
+      <?php print render($page['content_top']); ?>
+
+      <div id="main">
+        <?php if ($page['highlighted']): ?>
+          <div id="highlighted">
+            <?php print render($page['highlighted']); ?>
+          </div><!-- /#highlighted -->
+        <?php endif; ?>
+         
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?>
+          <h1 class="title" id="page-title"><?php print $title; ?></h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php if ($tabs): ?>
+          <div class="tabs"><?php print render($tabs); ?></div>
+        <?php endif; ?>
+        <?php print $messages; ?>
+        <?php print render($page['help']); ?>
+        <?php if ($action_links): ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+
+        <?php print render($page['content']); ?>
+        <?php print render($page['content_bottom']); ?>
+
+        <?php print $feed_icons; ?>
+      </div><!-- /#main -->
+
+      <?php if ($page['sidebar_first']): ?>
+        <aside id="sidebar-first" role="complementary">
+          <?php print render($page['sidebar_first']); ?>
+        </aside><!-- /#sidebar-first -->
       <?php endif; ?>
 
-      <?php if ($site_slogan): ?>
-        <div id="site-slogan"><?php print $site_slogan; ?></div>
+      <?php if ($page['sidebar_second']): ?>
+        <aside id="sidebar-second" role="complementary">
+          <?php print render($page['sidebar_second']); ?>
+        </aside><!-- /#sidebar-second -->
       <?php endif; ?>
 
-      <?php print render($page['header']); ?>
-    </header><!-- /#header -->
-  </div></div>
-<?php endif; ?>
-
-<div id="page">
-  <?php if ($page['featured']): ?>
-    <div id="featured">
-      <?php print render($page['featured']); ?>
-    </div><!-- /#featured -->
-  <?php endif; ?>
-
-  <?php print render($page['content_top']); ?>
-
-  <div id="main">
-    <?php if ($page['highlighted']): ?>
-      <div id="highlighted">
-        <?php print render($page['highlighted']); ?>
-      </div><!-- /#highlighted -->
-    <?php endif; ?>
-     
-    <?php print render($title_prefix); ?>
-    <?php if ($title): ?>
-      <h1 class="title" id="page-title"><?php print $title; ?></h1>
-    <?php endif; ?>
-    <?php print render($title_suffix); ?>
-    <?php if ($tabs): ?>
-      <div class="tabs"><?php print render($tabs); ?></div>
-    <?php endif; ?>
-    <?php print $messages; ?>
-    <?php print render($page['help']); ?>
-    <?php if ($action_links): ?>
-      <ul class="action-links"><?php print render($action_links); ?></ul>
-    <?php endif; ?>
-
-    <?php print render($page['content']); ?>
-    <?php print render($page['content_bottom']); ?>
-
-    <?php print $feed_icons; ?>
-  </div><!-- /#main -->
-
-  <?php if ($page['sidebar_first']): ?>
-    <aside id="sidebar-first" role="complementary">
-      <?php print render($page['sidebar_first']); ?>
-    </aside><!-- /#sidebar-first -->
-  <?php endif; ?>
-
-  <?php if ($page['sidebar_second']): ?>
-    <aside id="sidebar-second" role="complementary">
-      <?php print render($page['sidebar_second']); ?>
-    </aside><!-- /#sidebar-second -->
-  <?php endif; ?>
-
-</div><!-- /#page -->
-
+    </div><!-- /#page -->
+    <div class="push"></div>
+  </div>
 <?php if ($page['footer']): ?>
   <footer id="footer" role="contentinfo">
     <div class="wrapper"><div class="inner">
